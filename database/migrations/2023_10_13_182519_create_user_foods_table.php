5721d59af227892670b83ad9b9bca40d2aa46bc0
FizKw <hafizhkumara.w@gmail.com>
2023-10-15 20:28:20 +0700

    modify database a little

--- database/migrations/2023_10_13_182519_create_user_foods_table.php
+++ database/migrations/2023_10_13_182519_create_user_foods_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('foods_id')->constrained();
-            $table->integer('count')->default(1);
             $table->timestamps();
         });
     }
