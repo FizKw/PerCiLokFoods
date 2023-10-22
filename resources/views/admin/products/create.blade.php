210bd133a37805e8750fd23b22c64f2e4794e04c
FizKw <hafizhkumara.w@gmail.com>
2023-10-23 01:48:07 +0700

    create layout

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,36 +1,41 @@
-  
-  
 <x-app-layout>
-    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data" >
-        @csrf
-        <div class="grid md:grid-cols-2 gap-4 pt-10 place-items-center container">
-            <div>
-                <input type="text" name="food" class="form-control" placeholder="Food">
-            </div>
-            <div>
-                <input type="text" name="price" class="form-control" placeholder="Price" required>
-            </div>
-
-                <!-- <input type="text" name="category" class="form-control" placeholder="Category" required> -->
-            <div class="input-group">
-                <select class="select select-bordered" name="category" required>
-                    <option disabled selected hidden value="">Category</option>
-                    <option value="Makanan">Makanan</option>
-                    <option value="Minuman">Minuman</option>
-                    <option value="Snack">Snack</option>
-                </select>
-            </div>
-            <div>
-                <textarea class="form-control" name="description" placeholder="Descriptoin" required></textarea>
-            </div>
-
-            <div>
-                <input type="file" name="food_image" placeholder="Food Image" required>
-            </div>
-
-            <div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+    <section class="py-12 px-4 lg:px-0 container mx-auto">
+        <h2 class="text-center font-bold text-2xl p-3">Create Menu</h2>
+        <div class="p-12 sm:p-8 bg-color4 shadow rounded-lg">
+            <div class="max-w-xl">
+                <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <x-input-label class="form-label">Image</x-input-label>
+                    <div class="mt-4">
+                        <x-text-input type="file" name="food_image" placeholder="Food Image" />
+                        <x-primary-button>Add Image</x-primary-button>
+                    </div>
+                    <div class="mb-3">
+                        <x-input-label class="form-label">Food</x-input-label>
+                        <x-text-input type="text" name="food" class="mt-1 block w-full" placeholder="Food"/>
+                    </div>
+                    <div class="mb-3">
+                        <x-input-label class="form-label">Price</x-input-label>
+                        <x-text-input type="text" name="price" class="mt-1 block w-full" placeholder="Price"/>
+                    </div>
+                    <div class="input-group">
+                        <select class="select select-bordered" name="category" required>
+                            <option disabled selected hidden value="">Category</option>
+                            <option value="Makanan">Makanan</option>
+                            <option value="Minuman">Minuman</option>
+                            <option value="Snack">Snack</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <x-input-label class="mt-1 ">Description</x-input-label>
+                        <x-text-input class="mt-1 block w-full" name="description" placeholder="Food Description"/>
+                    </div>
+                    <div class="">
+                        <x-primary-button class="btn btn-warning">Create</x-primary-button>
+                    </div>
+                </form>
             </div>
         </div>
-    </form>
+    </section>
 </x-app-layout>
+
