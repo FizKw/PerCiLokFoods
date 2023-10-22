6ef7d085fe6de0f061f6a7004582d67cc15f209e
Soridiii <lutfisalinggih@gmail.com>
2023-10-22 20:48:32 +0700

    po

--- resources/views/components/foodlist.blade.php
+++ resources/views/components/foodlist.blade.php
@@ -1,3 +1,3 @@
-<div  {{ $attributes->merge(['class' => 'py-12 px-auto container mx-auto sm:grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xs:hidden gap-7']) }}>
+<div  {{ $attributes->merge(['class' => 'py-12 container place-items-center  mx-auto grid grid-flow-cols grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10']) }}>
     {{ $slot }}
 </div>
