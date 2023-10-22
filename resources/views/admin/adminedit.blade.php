2dd87274a3f0e6a3026c03b20fc369154be783fa
Soridiii <lutfisalinggih@gmail.com>
2023-10-22 20:49:59 +0700

    p

--- resources/views/admin/adminedit.blade.php
+++ resources/views/admin/adminedit.blade.php
@@ -1,52 +1,64 @@
-<div class="p-6 text-gray-900 dark:text-gray-100">
-    <div class="d-flex align-items-center justify-content-between">
-        <h1 class="mb-0">List Product</h1>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">Add Product</a>
+<div class="p-6 text-gray-900">
+    <div class="flex-none text-center">
+        <h1 class="flex-center ">List Product</h1>
+        <a href="{{ route('products.create') }}" class="justify-items-end btn rounded-full bg-color1 hover:bg-red-400 text-white">Add list</a>
+
     </div>
-    <hr />
     @if(Session::has('success'))
         <div class="alert alert-success" role="alert">
             {{ Session::get('success') }}
         </div>
     @endif
-    <table class="table table-hover">
-        <thead class="table-primary">
-            <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Price</th>
-                <th>Product Code</th>
-                <th>Description</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>+
-            @if($product->count() > 0)
-                @foreach($product as $rs)
+    <div class="overflow-x-auto h-96">
+        <table class="table table-hover">
+                <thead class="text-center">
                     <tr>
-                        <td class="align-middle">{{ $loop->iteration }}</td>
-                        <td class="align-middle">{{ $rs->food }}</td>
-                        <td class="align-middle">{{ $rs->price }}</td>
-                        <td class="align-middle">{{ $rs->category }}</td>
-                        <td class="align-middle">{{ $rs->description }}</td>  
-                        <td class="align-middle">
-                            <div class="btn-group" role="group" aria-label="Basic example">
-                                <a href="{{ route('products.show', $rs->id) }}" type="button" class="btn btn-secondary">Detail</a>
-                                <a href="{{ route('products.edit', $rs->id)}}" type="button" class="btn btn-warning">Edit</a>
-                                <form action="{{ route('products.destroy', $rs->id) }}" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger m-0">Delete</button>
-                                </form>
-                            </div>
-                        </td>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Product Code</th>
+                        <th>Description</th>
+                        <th>Action</th>
                     </tr>
-                @endforeach
-            @else
-                <tr>
-                    <td class="text-center" colspan="5">Product not found</td>
-                </tr>
-            @endif
-        </tbody>
-    </table>
+                </thead>
+                <tbody>
+                    @if($product->count() > 0)
+                        @foreach($product as $rs)
+                            <tr>
+                                <td class="align-middle text-center">{{ $loop->iteration }}</td>
+                                <td class="align-middle text-center">{{ $rs->food }}</td>
+                                <td class="align-middle text-center">{{ $rs->price }}</td>
+                                <td class="align-middle text-center">{{ $rs->category }}</td>
+                                <td class="align-middle text-center">{{ $rs->description }}</td>  
+                                <td class="align-middle text-center">
+                                    <div class="btn-group" role="group" aria-label="Basic example">
+                                        <a href="{{ route('products.show', $rs->id) }}" type="button" class="btn btn-square btn-outline">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                            </svg>
+                                        </a>
+                                        <a href="{{ route('products.edit', $rs->id)}}" type="button" class="btn btn-square btn-outline">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </a>
+                                        <form action="{{ route('products.destroy', $rs->id) }}" method="POST" type="button" class="btn p-0" onsubmit="return confirm('Delete?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-square btn-outline">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td class="text-center" colspan="5">Product not found</td>
+                        </tr>
+                    @endif
+             </tbody>
+         </table>
+    </div>
 </div>
