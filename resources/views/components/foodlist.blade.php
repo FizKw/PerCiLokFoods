19888e444d1e5afa0d4bd258d5685ad5463240f3
Soridiii <lutfisalinggih@gmail.com>
2023-10-22 18:35:12 +0700

    s

--- resources/views/components/foodlist.blade.php
+++ resources/views/components/foodlist.blade.php
@@ -1,3 +1,3 @@
-<div  {{ $attributes->merge(['class' => 'py-12  container mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7']) }}>
+<div  {{ $attributes->merge(['class' => 'py-12 px-auto container mx-auto sm:grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xs:hidden gap-7']) }}>
     {{ $slot }}
 </div>
--- resources/views/user/userhome.blade.php
+++ resources/views/user/userhome.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-    
+    <div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Best food to fulfill your belly.</h1>
+            <button class="btn rounded-full bg-color1 hover:bg-red-400 text-white">Get Started</button>
+          </div>
+        </div>
+      </div>  
         @if($product->count() > 0)
         <x-foodlist>
         @foreach($product as $rs)
@@ -8,11 +16,12 @@
                       <img src="{{ asset('storage/' . $rs->food_image) }}" alt="{{ $rs->food }}"  class="object-fill w-96 h-56 rounded-xl" />
                     </figure>
                     <div class="card-body items-center text-center">
-                        <h2 class="card-title">
+                        <h2 class="card-title text-2xl">
                             {{ $rs->food }}
                             <div class="badge badge-sm bg-color3">{{ $rs->category }}</div>
                         </h2>
                       <p>{{ $rs->description }}</p>
+                      <h2 class="text-gray-900 text-xl font-bold">Rp{{ $rs->price }}</h2>
                       <div class="card-action flex px-auto">
                         <button><a href="{{ route('products.show', $rs->id) }}" type="button" class=" flex btn rounded-full bg-color2 hover:text-black">See details</a></button>
                         <button><a href="{{ route('addtocart', $rs->id) }}" type="button" class="btn rounded-full bg-color1 text-white hover:text-black active:text-black">Add to cart</a></button>
