bec53fcd7691681a563aa3bf627a03e1197247b5
FizKw <hafizhkumara.w@gmail.com>
2023-10-22 21:45:18 +0700

    dropdown for creating menu category

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,41 +1,36 @@
   
-@section('title', 'Create Product')
   
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Add Product
-        </h2>
-    </x-slot>
-    <hr />
-    
-    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data" >
         @csrf
-        <div class="row mb-3">
-            <div class="col">
+        <div class="grid md:grid-cols-2 gap-4 pt-10 place-items-center container">
+            <div>
                 <input type="text" name="food" class="form-control" placeholder="Food">
             </div>
-            <div class="col">
+            <div>
                 <input type="text" name="price" class="form-control" placeholder="Price" required>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col">
-                <input type="text" name="category" class="form-control" placeholder="Category" required>
+
+            <div>
+                <!-- <input type="text" name="category" class="form-control" placeholder="Category" required> -->
+                <div class="input-group">
+                    <select class="select select-bordered" name="category" required>
+                        <option disabled selected hidden value="">Category</option>
+                        <option value="Makanan">Makanan</option>
+                        <option value="Minuman">Minuman</option>
+                        <option value="Snack">Snack</option>
+                    </select>
+                </div>
             </div>
-            <div class="col">
+            <div>
                 <textarea class="form-control" name="description" placeholder="Descriptoin" required></textarea>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col">
+
+            <div>
                 <input type="file" name="food_image" placeholder="Food Image" required>
             </div>
-        </div>
-        
- 
-        <div class="row">
-            <div class="d-grid">
+
+            <div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
