03df2478a0416ab1e128f325c26a9e38b3aad487
FizKw <hafizhkumara.w@gmail.com>
2023-10-24 21:27:59 +0700

    logo & title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'PerCiLok Foods') }}</title>
+        <link rel="icon" href="{!! asset('percilok.png') !!}"/>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'PerCiLok Foods') }}</title>
+        <link rel="icon" href="{!! asset('percilok.png') !!}"/>
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
